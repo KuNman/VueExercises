2e1c8e57366ee590c403a11d30317f55b96fc21b
KuNman <knejman56@gmail.com>
2018-06-17 18:07:52 +0200

    Bus full of events

---
+++ Component/Ex2/src/components/Server/ServerDetails.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+      <p v-if="!server">Please select a Server</p>
+      <p v-else>Server : {{server.id}}, status {{server.status}}</p>
+      <button @click="resetStatus">Reset status</button>
+    </div>
+
+</template>
+
+<script>
+import { serverBus } from '../../main.js'
+
+export default {
+  data: function() {
+    return {
+      server: false
+    }
+  },
+  created() {
+    serverBus.$on('serverSelected', (server) => {
+      this.server = server;
+    });
+  },
+  methods: {
+    resetStatus() {
+        this.server.status = 'Normal';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Component/Ex2/src/components/Server/Servers.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <server v-for="server in servers" :server="server"></server>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { serverBus } from '../../main.js';
+import Server from './Server.vue';
+
+export default {
+  data: function() {
+    return {
+      servers: [
+        { id: 1, status: 'Normal'},
+        { id: 2, status: 'Critical'},
+        { id: 3, status: 'Unknown'},
+        { id: 4, status: 'Normal'},
+      ]
+    };
+  },
+  components: {
+    Server
+  },
+  methods: {
+
+  }
+}
+</script>
+<style>
+
+</style>
