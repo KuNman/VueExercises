40e81182a69226c73849c2341ca9f391ee11ccda
KuNman <knejman56@gmail.com>
2018-06-17 23:52:02 +0200

    Dynamic components with slots

---
+++ Component/Ex3/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="app = 'app-blue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="app = 'app-green'" class="btn btn-success">Load Green Template</button>
+                <button @click="app = 'app-red'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <components :is="app">
+                  <p>Active component is {{app}}</p>
+                </components>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function() {
+          return {
+            app: 'app-blue'
+          }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
