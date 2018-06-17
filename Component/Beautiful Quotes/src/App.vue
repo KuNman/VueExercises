55b10b9d55ba84d2963f2d6925e164d15fa70e5b
KuNman <knejman56@gmail.com>
2018-06-18 00:47:23 +0200

    Beatufil quotes
    Few components reacting with each other

---
+++ Component/Beautiful Quotes/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <bar :quotesAmount="quotes.length" :maxQuotes="maxQuotes"></bar>
+        <newQuote @quoteAdded="newQuote"></newQuote>
+        <quotesGrid :quotes="quotes" @deleteQuote="deleteQuote"></quotesGrid>
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <div class="alert alert-info">
+              Click on Quote to delete it!
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuotesGrid from './components/QuotesGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Bar from './components/Bar.vue';
+
+    export default {
+        data: function () {
+          return {
+            quotes: ['Demo quote'],
+            maxQuotes: 10,
+          }
+        },
+        methods: {
+          newQuote(quote) {
+            if(this.quotes.length >= this.maxQuotes) {
+              return alert('Maximum amount of quotes!')
+            }
+            this.quotes.push(quote)
+          },
+          deleteQuote(i) {
+            this.quotes.splice(i, 1)
+          }
+        },
+        components: {
+          'quotesGrid': QuotesGrid,
+          'newQuote' : NewQuote,
+          'bar' : Bar
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Component/Beautiful Quotes/src/components/QuotesGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <quote v-for="(quote, i) in quotes" @click.native="deleteQuote(i)">{{ quote }}</quote>
+  </div>
+</template>
+<script>
+import Quote from './Quote.vue';
+
+export default {
+  props: ['quotes'],
+  methods: {
+    deleteQuote(i) {
+      this.$emit('deleteQuote', i);
+    }
+  },
+  components: {
+    Quote
+  }
+}
+</script>
+<style></style>
