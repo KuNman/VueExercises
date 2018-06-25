123a4f422eb337260648e0a51eb7c29f2e2459d1
KuNman <knejman56@gmail.com>
2018-06-26 01:14:57 +0200

    Some router examples

--- Router/Ex1/src/components/user/User.vue
+++ Router/Ex1/src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <button @click="backToHome" class="btn btn-primary">Go to home</button>
+    <hr />
+    <router-view></router-view>
+  </div>
 </template>
+<script>
+  export default {
+    methods: {
+      backToHome() {
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
--- Router/Ex1/src/components/user/UserDetail.vue
+++ Router/Ex1/src/components/user/UserDetail.vue
@@ -1,3 +1,9 @@
 <template>
+  <div>
     <h3>Some User Details</h3>
+    <p>
+      Loaded user : {{ $route.params.id }}
+    </p>
+
+  </div>
 </template>
