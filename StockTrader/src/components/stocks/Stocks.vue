2181610b93b79a90ccf033776d8817b73d47dda5
KuNman <knejman56@gmail.com>
2018-07-01 14:41:24 +0200

    Buying works

--- StockTrader/src/components/stocks/Stocks.vue
+++ StockTrader/src/components/stocks/Stocks.vue
@@ -8,17 +8,13 @@
   import Stock from './Stock.vue'
 
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 100},
-          { id: 2, name: 'Apple', price: 150},
-          { id: 3, name: 'Google', price: 180},
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     }
   }
 </script>
---
+++ StockTrader/src/data/stocks.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, name: 'BMW', price: 100},
+  { id: 2, name: 'Apple', price: 150},
+  { id: 3, name: 'Google', price: 180},
+]
